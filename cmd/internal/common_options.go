6414017a473173477d8c51520af772c5f919effb
Paul Serice <paul@serice.net>
2024-04-19 00:43:45 -0500

    Add options.xml file.
    Common command-line options can be stored instead in the options.xml
    file so they do not have to be specified on the command-line each time
    a command is invoked.

---
+++ cmd/internal/common_options.go
@@ -0,0 +1,65 @@
+// Data structure used to collect options from config.xml and from the
+// command line.
+
+package internal
+
+import (
+	"flag"
+)
+
+// CommonOptions holds the command-line options and options in the
+// options.xml that are need by most of our commands.
+type CommonOptions struct {
+	
+	// AuthFileName is an alternative file name for auth.xml.
+	AuthFileName string `xml:"auth-file-name"`
+	
+	// BaseURL is the base URL for connecting to Gitlab REST endpoints.
+	BaseURL string `xml:"base-url"`
+
+	// OptionsFileName is an alternative file name for options.xml.
+	// Note that the user can only change this option on the command
+	// line, not in the options.xml file (because it leads to circular
+	// logic having the user specify the location of the options.xml
+	// file in the options.xml file).
+	OptionsFileName string `xml:"optons-file-name,omitempty"`
+}
+
+// Initialize initializes this CommonOptions instance for use the the
+// "flag" package in order to parse command-line options.
+func (opts *CommonOptions) Initialize() {
+
+	// Set default values that differ from the zero defaults.
+	if opts.AuthFileName == "" {
+		opts.AuthFileName = "auth.xml"
+	}
+	if opts.BaseURL == "" {
+		opts.BaseURL = "https://gitlab.com/"
+	}
+	if opts.OptionsFileName == "" {
+		opts.OptionsFileName = "options.xml"
+	}
+
+	// --auth
+	flag.StringVar(
+		&opts.AuthFileName,
+		"auth",
+		"auth.xml",
+		"name of XML file with authentication information")
+
+	// --base-url
+	flag.StringVar(
+		&opts.BaseURL,
+		"base-url",
+		"https://gitlab.com/",
+		"base URL for Gitlab REST endpoints which should not include " +
+			"the \"api/v4\" suffix")
+
+	// --options
+	flag.StringVar(
+		&opts.OptionsFileName,
+		"options",
+		"options.xml",
+		"name of XML file with default options")
+}
+
--- cmd/list_projects/main.go
+++ cmd/list_projects/main.go
@@ -1,46 +1,107 @@
 package main
 
 import(
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"encoding/xml"
 
 	"github.com/jalitriver/gitlab-cmds/cmd/internal"
 	"github.com/xanzy/go-gitlab"
 )
 
+// Options holds the command-line options.
+type Options struct {
+	internal.CommonOptions
+}
+
+// Initialize initializes this Options instance by parsing the
+// command-line arguments to find the location of options.xml file
+// which might have been specified on the command-line. It then reads
+// the options.xml file to initialize this Options instance.  Because
+// command-line options take precedence over options in the
+// options.xml file, it is necessary for the call to call flag.Parse()
+// a second time.
+func (opts *Options) Initialize() {
+
+	// Inform the "flag" package where it should store the
+	// command-line options.
+	opts.CommonOptions.Initialize()
+
+	// Parse the command-line options primarily looking for an
+	// alternative location for the options.xml file which might have
+	// been specified on the command line.
+	flag.Parse()
+
+	// Try to open the options.xml file.
+	f, err := os.Open(opts.OptionsFileName)
+	if err != nil {
+		// Squash the error.  The user is not required to have an
+		// options.xml file.
+		return
+	}
+	defer f.Close()
+
+	// Try to read the options.xml file.
+	err = xml.NewDecoder(f).Decode(&opts)
+	if err != nil {
+		log.Fatalf(
+			"unable to read options from %q: %v",
+			opts.OptionsFileName,
+			err)
+	}
+}
+
 func main() {
 
-	// FIXME: Should be option in config.xml.
-	baseURL := "https://gitlab.serice.net/api/v4"
-	
-	// FIXME: Should be option in config.xml.
-	fname := "/home/pserice/src/go/gitlab-cmds.git/auth.xml"
+	// Find the base name for the executable.
+	basename := filepath.Base(os.Args[0])
+
+	// Usage.
+	flag.Usage = func() {
+		out := flag.CommandLine.Output()
+		fmt.Fprintf(out, "\n")
+		fmt.Fprintf(out, "Usage: %s [options] [<file> ...]\n", basename)
+		fmt.Fprintf(out, "\n")
+		fmt.Fprintf(out, "    List Gitlab Projects\n")
+		fmt.Fprintf(out, "\n")
+		fmt.Fprintf(out, "Options:\n")
+		fmt.Fprintf(out, "\n")
+		flag.PrintDefaults()
+	}
+
+	// Parse command-line arguments.
+	opts := new(Options)
+	opts.Initialize()
+	flag.Parse()
 
 	// Load the authentication information from file.
-	authInfo, err := internal.LoadAuthInfo(fname)
+	authInfo, err := internal.LoadAuthInfo(opts.AuthFileName)
 	if err != nil {
 		log.Fatalf(
 			"LoadAuthInfo: Unable to load authentication information " +
-			"from file %v: %v", fname, err)
+			"from file %v: %v", opts.AuthFileName, err)
 	}
 
 	// Create the Gitlab client based on the authentication
 	// information provided by the user.
 	client, err := authInfo.CreateGitlabClient(
-		gitlab.WithBaseURL(baseURL))
+		gitlab.WithBaseURL(opts.BaseURL))
 	if err != nil {
 		log.Fatalf("CreateGitlabClient: %v\n", err)
 	}
 
 	// Get the list of projects.
-	opts := gitlab.ListProjectsOptions{}
-	ps, _, err := client.Projects.ListProjects(&opts)
+	listProjOpts := gitlab.ListProjectsOptions{}
+	ps, _, err := client.Projects.ListProjects(&listProjOpts)
 	if err != nil {
 		log.Fatalf("ListProjects: %v\n", err)
 	}
 
 	// Print each project.
 	for _, p := range ps {
-		fmt.Printf("%v\n", p)
+		fmt.Printf("%v: %v\n", p.ID, p.WebURL)
 	}
 }
